998d612644637f963a40fb8d1b8398732f704226
Lyrica Prismriver <lyricaprismriver@hotmail.com>
2019-01-10 07:30:06 +0800

    Initialization
    initialize on 2019/01/10

---
+++ Ch03_Objects, Types, and Values/3.2-Variables/source01.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	string name = "Annemarie";
+	int number_of_steps = 39;
+
+//	string name2 = 39;
+//	int number_of_steps2 = "Annemarie";
+
+    cout << name << "\n";
+    cout << number_of_steps << "\n";
+
+//    cout << name2 << "\n";
+//    cout << number_of_steps2 << "\n";
+
+	return 0;
+}
